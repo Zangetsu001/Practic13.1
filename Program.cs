d720940bd6e7fd0f2c49fe6e81f83ca519a5f122
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2024-11-26 14:34:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic13._2
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int a = Input("Введите a:");
+            int b = Input("Введите b:");
+            int c = Input("Введите c:");
+            int result = Min(Min(a, b), c);
+            Output(result);
+            Console.Read();
+
+        }
+        public static int Input(string message)
+        {
+            Console.Write(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        public static int Min(int a, int b)
+        {
+            return a < b ? a : b;
+        }
+       
+        public static void Output(int result)
+        {
+            Console.WriteLine(result);
+        }
+
+    }
+}
