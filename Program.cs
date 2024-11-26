4066e5f8e35722b0af1cb04d4ae59479d30a6859
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2024-11-26 13:56:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        /// <summary>
+        /// Найти минимальную величину из двух целых переменных 
+        /// a, b, используя вспомогательные методы 
+        /// (ввода аргументов, нахождения минимальной величины, вывода результата).
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int a = Input("Введите a: ");
+            int b = Input("Введите b: ");
+            int result = Min(a, b);
+            Output(result);
+            Console.ReadKey();
+        }
+        public static int Input(string message)
+        {
+            Console.Write(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        public static int Min(int a, int b)
+        {
+            return a < b ? a : b;
+        }
+        public static void Output(int result)
+        {
+            Console.Write($"result: {result}");
+        }
+    }
+}
