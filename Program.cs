36366413f4a52a6638608e239faaffc7ab39e83e
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2024-11-26 14:40:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic13._3
+{
+    class Program
+    {
+        /// <summary>
+        /// Найти s=min(a, b) + min(c, d), 
+        /// используя вспомогательные функции
+        /// (ввода аргументов, нахождения минимальной величины, вывода результата).
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int a = Input("Введите a: ");
+            int b = Input("Введите b: ");
+            int c = Input("Введите c: ");
+            int d = Input("Введите d: ");
+            int result = Min1(a, b) - Min2(c, d);
+            Output(result);
+            Console.ReadKey();
+        }
+        public static int Input(string message)
+        {
+            Console.Write(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        public static int Min1(int a, int b)
+        {
+            return a < b ? a : b;
+        }
+        public static int Min2(int c, int d)
+        {
+            return c < d ? c : d;
+        }
+        public static void Output(int result)
+        {
+            Console.WriteLine(result);
+        }
+
+    }
+}
